4d9774462506a7ece89e0f30109434abe2d4b83f
jmcotelo <jmcotelo@gmail.com>
2013-12-17 19:19:03 +0100

    Added a queue pusher listener implementation

--- acequia/twitter/listeners/__init__.py
+++ acequia/twitter/listeners/__init__.py
@@ -5,6 +5,6 @@ except ImportError:
 	print("WARNING: Tweepy not found. Tweepy-based listeners won't be loaded")
 
 try:
-	from .twython import TwythonDummyStreamListener
+	from .twython import TwythonDummyStreamListener, TwythonQueuePusherStreamListener
 except ImportError:
 	print("WARNING: Twython not found. Twython-based listeners won't be loaded")
