d56740a9338c0a892be521df256715df986b3028
jmcotelo <jmcotelo@gmail.com>
2013-12-20 13:35:20 +0100

    Added adaptive retrieval stub for integration described in #5

--- acequia/twitter/__init__.py
+++ acequia/twitter/__init__.py
@@ -1,9 +1,10 @@
-__all__ = ["listeners", "writer","interfaces","dumpers", "fetchers", "graph", "parsing"]
+__all__ = ["listeners", "writer","interfaces","dumpers", "fetchers", "graph", "parsing", "retrieval"]
 from . import listeners
 from . import writer
 from . import interfaces
 from . import dumpers
 from . import parsing
-from .fetchers import TwitterStreamingFetcher
 from . import subprocess
 from . import graph
+from .retrieval import TwitterAdaptiveRetriever
+from .fetchers import TwitterStreamingFetcher
---
+++ acequia/twitter/retrieval.py
@@ -0,0 +1,8 @@
+'''
+Created on Dec 20, 2012
+
+@author: jcm
+'''
+
+class TwitterAdaptiveRetriever:
+	pass
